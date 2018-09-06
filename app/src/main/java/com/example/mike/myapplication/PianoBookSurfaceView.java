c0358801e67ef80934feffd8da29b062b7743e5d
Mike-Heneghan <m.heneghan@live.co.uk>
2018-09-06 09:54:39 +0100

    Added surface view class

---
+++ app/src/main/java/com/example/mike/myapplication/PianoBookSurfaceView.java
@@ -0,0 +1,22 @@
+package com.example.mike.myapplication;
+
+import android.app.Activity;
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+public class PianoBookSurfaceView extends Activity {
+
+    private GLSurfaceView mGLView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        mGLView = new GLSurfaceView(this);
+        setContentView(mGLView);
+    }
+
+}
